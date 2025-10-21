195aa2d716506f3845fe4c14b87b1101cef9c8df
LucaCDRocha <lucadrocha@gmail.com>
2025-10-21 12:02:27 +0200

    Add the mechanism to add planets on clic

--- code/2025-10-21/planets/sketch.js
+++ code/2025-10-21/planets/sketch.js
@@ -51,11 +51,18 @@ class Rocket {
     vertex(-this.size * 0.3, 0);
     vertex(-this.size * 0.5, this.size * 0.5);
     endShape(CLOSE);
-    
+
     if (keyIsDown(UP_ARROW)) {
       fill(255, 100, 0);
       noStroke();
-      triangle(-this.size * 0.5, -this.size * 0.2, -this.size * 0.9, 0, -this.size * 0.5, this.size * 0.2);
+      triangle(
+        -this.size * 0.5,
+        -this.size * 0.2,
+        -this.size * 0.9,
+        0,
+        -this.size * 0.5,
+        this.size * 0.2
+      );
     }
     pop();
   }
@@ -76,7 +83,7 @@ function setup() {
     { distance: 180, size: 40, speed: 0.008, color: color(150, 100, 250) }, // Venus
     { distance: 250, size: 45, speed: 0.006, color: color(100, 255, 150) }, // Earth
     { distance: 320, size: 35, speed: 0.005, color: color(255, 100, 100) }, // Mars
-    { distance: 400, size: 60, speed: 0.003, color: color(255, 220, 100) }  // Jupiter
+    { distance: 400, size: 60, speed: 0.003, color: color(255, 220, 100) }, // Jupiter
   ];
 
   for (let data of planetData) {
@@ -104,4 +111,13 @@ function draw() {
 
   rocket.update();
   rocket.draw();
+}
+
+// every time we click it add a new planet at the mouse position that orbit around the sun
+function mousePressed() {
+  let distance = dist(mouseX, mouseY, sun.x, sun.y);
+  let size = random(20, 60);
+  let speed = random(0.002, 0.01);
+  let planetColor = color(random(50, 255), random(50, 255), random(50, 255));
+  planets.push(new Planet(distance, size, speed, planetColor));
 }
